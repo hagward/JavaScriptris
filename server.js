408a58f950e131d5982d685aa1f1f5fd89c9c738
rattmuffen <rattmuffen@gmail.com>
2013-06-29 16:00:09 +0200

    Servin' that server!
    Server now actually uses socket.io for something!
    
    +Server now keeps track of, and emits, number of connected clients in
    real time.
    +Client displays the total number of connected clients.
    +Added second game canvas just for fun.
    +Added version number to package.json (fixed a npm install error I ran
    into)

--- server.js
+++ server.js
@@ -2,14 +2,50 @@ var io = require('socket.io');
 var express = require('express');
 var UUID = require('node-uuid');
 
+var port = 1427;
+
+//Start express server
 var server = express.createServer();
-io.listen(server);
+server.listen(port);
 
-server.use(express.logger());
+var numberOfClients = 0;
 
-server.get('/', function(req, res){
-    res.send('Hello World!');
+console.log('Server listening on port ' + port);
+
+//Forward to game.html
+server.get('/', function(req, res) { 
+    res.sendfile( __dirname + '/game.html');
 });
 
-server.listen();
-console.log('Express server started on port %s', server.address().port);
+//Serve other files
+server.get('/*', function(req, res, next) {
+    res.sendfile( __dirname + '/' + req.params[0]);
+}); 
+
+
+//Start socket.io server
+var sio = io.listen(server);
+
+sio.sockets.on('connection', function (client) {
+    numberOfClients++;
+
+    //Generate ID for new client
+    client.userid = UUID();
+
+    //emit ID to client
+    client.emit('onconnected', { id: client.userid } );
+
+    console.log('Client connected ' + client.userid);
+
+    //On client disconnect:
+    client.on('disconnect', function () {
+        //update and emit number of clients
+        numberOfClients--;
+        sio.sockets.emit('message', {clients: numberOfClients});
+
+        console.log('Client disconnected ' + client.userid );
+    }); 
+
+    //Emit number of clients to everyone
+    sio.sockets.emit('message', {clients: numberOfClients});
+});
